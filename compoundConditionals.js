0efe03171eac5b4ff3d452f8c735d9917cf7900e
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2023-11-02 21:47:33 +0000

    All Work in Class

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -4,8 +4,49 @@
 
 
 // Change these values to test different scenarios
-let mood = "sad";        // Possible values: "happy", "sad", "excited", "bored"
-let timeOfDay = "night"; // Possible values: "morning", "afternoon", "evening", "night"
+let rand = Math.floor(Math.random() * 5);
+
+let mood; // Possible values: "happy", "sad", "excited", "bored", "tired"
+switch (rand) {
+  case 0:
+    mood = "happy";
+    break;
+  case 1:
+    mood = "sad";
+    break;
+  case 2:
+    mood = "excited";
+    break;
+  case 3:
+    mood = "bored";
+    break;
+  case 4:
+    mood = "tired";
+    break;
+  default:
+    mood = "happy";
+    break;
+}
+
+rand = Math.floor(Math.random() * 4);
+let timeOfDay; // Possible values: "morning", "afternoon", "evening", "night"
+switch (rand) {
+  case 0:
+    timeOfDay = "morning";
+    break;
+  case 1:
+    timeOfDay = "afternoon";
+    break;
+  case 2:
+    timeOfDay = "evening";
+    break;
+  case 3:
+    timeOfDay = "night";
+    break;
+  default:
+    timeOfDay = "morning";
+    break;
+}
 
 let recommendedMovie;
 
@@ -14,8 +55,18 @@ let recommendedMovie;
 // - If the user is "excited" AND it's NOT "morning", recommend "High-Octane Thriller".
 // - If the user is "bored" AND it's "afternoon" OR "happy" AND it's "evening", recommend "Engaging Mystery Film".
 // - In all other cases, recommend "Popular Choice Film".
-
-
+if (mood === "tired" || timeOfDay === "night") {
+  recommendedMovie = "a Relaxing Ambient Film";
+}
+else if (mood === "excited" && timeOfDay !== "morning") {
+  recommendedMovie = "a High-Octane Thriller";
+}
+else if ((mood === "bored" && timeOfDay === "afternoon") || (mood === "happy" && timeOfDay === "evening")) {
+  recommendedMovie = "an Engaging Mystery Film";
+}
+else {
+  recommendedMovie = "a Popular Choice Film";
+}
 
 // 2. Console log the recommended movie
-
+console.log("You are " + mood + ". It is " + timeOfDay + ". I recommend watching " + recommendedMovie);
--- conditionals.js
+++ conditionals.js
@@ -3,17 +3,25 @@
 // recommends an ice cream flavor based on the current temperature in Fahrenheit.
 
 // 1. Create a variable named temperature any value between 32 and 68.
-
+let temperature = Math.floor((Math.random() * 59) + 32);
 
 // 2. Create a variable named recommendedFlavor. Do not assign a value.
-
+let recommendedFlavor;
 
 // 3. Implement the recommendation logic
 // - If the temperature is less than 50, recommend "Hot Chocolate Fudge".
 // - If the temperature is between 50 and 68 (inclusive), recommend "Caramel Crunch".
 // - If the temperature is greater than 68, recommend "Lemon Sorbet".
-
+if (temperature < 50) {
+  recommendedFlavor = "Hot Chocolate Fudge";
+}
+else if (temperature <= 68) {
+  recommendedFlavor = "Caramel Crunch"
+}
+else {
+  recommendedFlavor = "Lemon Sorbet";
+}
 
 // 4. Console log the recommended flavor.
-
+console.log("It is " + temperature + " degrees Farenheit today. I recommend getting the " + recommendedFlavor + " flavor");
 
